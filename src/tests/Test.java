200dc789bfb6446c6a0988941dd00f96415f7050
lwaxana <phj@controlz.be>
2014-05-29 18:51:56 +0200

    MAJ 29 05 2014

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -12,20 +12,20 @@ public class Test {
 		//String regex = "^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)+$";
 		//String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([@#$%^&+=]){0,}(?=\\S+$).{8,25}$";
 		//String regex = "^[a-zA-Z0-9]{2,}[a-zA-Z0-9@$-_]{0,}$";
-		String regex = "^[a-zA-Z]{1,}([a-zA-Z0-9@\\-_]){0,}";
-		
-		String str = "aaaa";
+		//String regex = "^[a-zA-Z]{1,}([a-zA-Z0-9@\\-_]){0,}";
+		//String regex = "^(0?\d|[12]\d|3[01])/(0?\d|1[012])/((?:19|20)\d{2})$";
+		//String str = "01/01/1900";
 		
 		
 	    
 		
 		
-		
+		/*
 		System.out.println(str.length());
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(str);
 		System.out.println(matcher.matches());
-		
+		*/
 		
 		
 		
